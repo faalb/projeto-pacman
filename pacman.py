915504960f4641dd5be14e1cbdbd68a36d19eb12
Francisco Anacleto de Albuquerque <104237733+faalb@users.noreply.github.com>
2023-06-06 15:49:23 -0300

    pacman.py

--- pacman.py
+++ pacman.py
@@ -2,50 +2,71 @@ if __name__ == '__main__':
     from random import randint
     import os
     from time import sleep
+    import winsound
 
-    MAPA = 20
+    TAMANHO_MAPA = 20
     PACMAN = ['O', 'o']
     COMIDA = 'X'
-    TEMPO_ESPERA = 0.3
-    posicao_pacman = [randint(0, MAPA - 1), randint(0, MAPA - 1)]
-    posicao_comida = [randint(0, MAPA - 1), randint(0, MAPA - 1)]
+    TEMPO_ESPERA = 0.1
     animacao = True
-    pontuacao = 0
+    pontos = 0
+    nivel = 1
+
+    def gerar_posicao():
+        return [randint(1, TAMANHO_MAPA - 2), randint(1, TAMANHO_MAPA - 2)]
+
+    pos_pacman = gerar_posicao()
+    pos_comida = gerar_posicao()
 
     def gerar_mapa():
         os.system('cls')
-        for i in range(MAPA):
-            for j in range(MAPA):
-                if i == posicao_pacman[0] and j == posicao_pacman[1]:
+        l = 0
+        while l < TAMANHO_MAPA:
+            c = 0
+            while c < TAMANHO_MAPA:
+                if l == pos_pacman[0] and c == pos_pacman[1]:
                     if animacao:
                         print(PACMAN[0], end='')
                     else:
                         print(PACMAN[1], end='')
-                elif i == posicao_comida[0] and j == posicao_comida[1]:
+                elif l == pos_comida[0] and c == pos_comida[1]:
                     print(COMIDA, end='')
                 else:
                     print('.', end='')
+                c += 1
             print()
-        print("Pontuação:", pontuacao)
+            l += 1
+
 
     def movimentar_pacman():
-        global posicao_pacman, posicao_comida, pontuacao
-        if posicao_pacman[0] < posicao_comida[0]:
-            posicao_pacman[0] += 1
-        elif posicao_pacman[0] > posicao_comida[0]:
-            posicao_pacman[0] -= 1
-        elif posicao_pacman[1] < posicao_comida[1]:
-            posicao_pacman[1] += 1
-        elif posicao_pacman[1] > posicao_comida[1]:
-            posicao_pacman[1] -= 1
+        if pos_pacman[0] < pos_comida[0]:
+            pos_pacman[0] += 1
+            winsound.Beep(440, 200)
+        elif pos_pacman[0] > pos_comida[0]:
+            pos_pacman[0] -= 1
+            winsound.Beep(440, 200)  
+        elif pos_pacman[1] < pos_comida[1]:
+            pos_pacman[1] += 1
+            winsound.Beep(440, 200)  
+        elif pos_pacman[1] > pos_comida[1]:
+            pos_pacman[1] -= 1
+            winsound.Beep(440, 200)  
 
-        if posicao_pacman == posicao_comida:
-            posicao_comida = [randint(0, MAPA - 1), randint(0, MAPA - 1)]
-            pontuacao += 1
 
+    print("Pontos: {}".format(pontos))
+    print("\nNível: {}".format(nivel))
 
     while True:
         gerar_mapa()
         movimentar_pacman()
+        if pos_pacman == pos_comida:
+            winsound.Beep(880, 500)  
+            pos_comida = gerar_posicao()
+            pontos += 1
+        if pontos == 11:
+            nivel += 1
+            pontos = 0
+            print("Parabéns! Você passou para o nível {}!".format(nivel))
+            sleep(3)
         animacao = not animacao
         sleep(TEMPO_ESPERA)
